26f289a46770a7af5845b1f08f88eb8793206b38
Mateo Olarte <mateo.olarte8@gmail.com>
2021-01-09 15:59:04 -0500

    add playground option

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import { schema } from "./schema";
 import { createContext } from "./context";
 
 const PORT = process.env.PORT || 4000;
-const server = new ApolloServer({ schema, context: createContext });
+const server = new ApolloServer({
+  schema,
+  context: createContext,
+  playground: true,
+});
 
 server.listen({ port: PORT }, () =>
   console.log(`Server ready at PORT: ${PORT} 🚀`)
