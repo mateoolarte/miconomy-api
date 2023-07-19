9de75a58bc92e7a40058599df8954a66aba078e0
Mateo Olarte <mateo.olarte8@gmail.com>
2023-07-19 01:37:42 -0500

    add unique validation on Budget name

--- src/graphql/Budgets/resolvers/createBudget.ts
+++ src/graphql/Budgets/resolvers/createBudget.ts
@@ -1,12 +1,21 @@
-import { checkUserAuth } from '../../../utils/checkUserAuth';
+import { checkUserAuth } from "../../../utils/checkUserAuth";
+import { throwError } from "../../../utils/throwError";
 
-import { parseBudgetCategories } from '../../shared/utils/parseBudgetCategories';
-import { parseBudgetSavings } from '../../shared/utils/parseBudgetSavings';
+import { parseBudgetCategories } from "../../shared/utils/parseBudgetCategories";
+import { parseBudgetSavings } from "../../shared/utils/parseBudgetSavings";
 
 export async function createBudgetResolver(db, user, args) {
   checkUserAuth(user);
 
   const { name } = args;
+
+  const budgetExist = await db.budget.findUnique({ where: { name } });
+
+  if (budgetExist) {
+    throwError("Ya tienes un presupuesto creado con este nombre", {
+      code: "BAD_USER_INPUT",
+    });
+  }
 
   const createdBudget = await db.budget.create({
     data: {
@@ -23,7 +32,7 @@ export async function createBudgetResolver(db, user, args) {
   const getCategories = createdBudget.categories;
   const getSavings = createdBudget.savings;
   const categories = getCategories.map(async (item) =>
-    parseBudgetCategories(item, db)
+    parseBudgetCategories(item, db),
   );
   const savings = getSavings.map(async (item) => parseBudgetSavings(item, db));
   const budget = {
